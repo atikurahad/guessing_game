fae6cc2b2894a5e7eef9618046d80df030bd0856
atikurahad <atikurahad@gmail.com>
2026-09-24 03:12:31 +0600

    std io with random number rand

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,18 @@ use std::io;
 
 fn main() {
     println!("Guess the number!");
+
+    // let secret_number = rand::rng().gen_range(1..=100);
+
     println!("Please input your guess: ");
 
+    let mut guess = String::new();
 
-    let guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read user input");
 
-    io::Stdin().read_line( guess)
+    let guess: u32 = guess.trim().parse().expect("Failed to parse");
+
+    println!("You guessed {guess}");
 }
